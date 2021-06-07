4d1338a45278364af54c6680929df2eb52468c58
Jung Younghwa <zseil@naver.com>
2021-06-07 23:00:15 +0900

    backend project initialization

---
+++ MK-backend/src/main/java/com/mk/MKBackendApplication.java
@@ -0,0 +1,12 @@
+package com.mk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MKBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MKBackendApplication.class, args);
+    }
+}
---
+++ MK-backend/src/test/java/com/mk/MKBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MKBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
